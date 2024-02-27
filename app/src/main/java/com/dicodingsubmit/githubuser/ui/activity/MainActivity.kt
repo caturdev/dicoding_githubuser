12325e5fa1701990e550e0b9c1ae57f596799fd5
Catur Wicaksono <bitcodenetwork@gmail.com>
2024-02-27 21:06:08 +0700

    update main activity and gradle

---
+++ app/src/main/java/com/dicodingsubmit/githubuser/ui/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dicodingsubmit.githubuser.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicodingsubmit.githubuser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+      
+  }
+}
