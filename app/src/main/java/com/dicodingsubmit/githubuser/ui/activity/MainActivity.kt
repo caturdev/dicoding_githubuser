47342b7aac399498ea8971e1a6559e4d37174018
Catur Wicaksono <bitcodenetwork@gmail.com>
2024-02-27 21:49:18 +0700

    update main activity with home fragment

--- app/src/main/java/com/dicodingsubmit/githubuser/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dicodingsubmit/githubuser/ui/activity/MainActivity.kt
@@ -2,7 +2,10 @@ package com.dicodingsubmit.githubuser.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.dicodingsubmit.githubuser.R
 import com.dicodingsubmit.githubuser.databinding.ActivityMainBinding
+import com.dicodingsubmit.githubuser.ui.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,15 @@ class MainActivity : AppCompatActivity() {
 
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
-      
+
+    val fragmentManager = supportFragmentManager
+    val homeFragment = HomeFragment()
+    val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+    if (fragment !is HomeFragment) {
+      fragmentManager.commit {
+        add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+      }
+    }
   }
 }
