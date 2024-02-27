744b0f8b9b1f43b6122281f8fe18504dd1dcb710
Catur Wicaksono <bitcodenetwork@gmail.com>
2024-02-27 21:48:18 +0700

    add home fragment

---
+++ app/src/main/java/com/dicodingsubmit/githubuser/ui/fragment/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.dicodingsubmit.githubuser.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dicodingsubmit.githubuser.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+  private lateinit var binding: FragmentHomeBinding
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    // init binding and inflate the layout for this fragment
+    binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.fab.setOnClickListener { v: View ->
+
+    }
+  }
+}
