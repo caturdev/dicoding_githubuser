a2fad6e1ae883dfd42ac5badf91b04fb832037f7
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-02-28 15:23:32 +0700

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,56 +1,69 @@
 plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
+	id("com.android.application")
+	id("org.jetbrains.kotlin.android")
+	id("kotlin-parcelize")
 }
 
 android {
-  namespace = "com.dicodingsubmit.githubuser"
-  compileSdk = 34
+	namespace = "com.dicodingsubmit.githubuser"
+	compileSdk = 34
 
-  defaultConfig {
-    applicationId = "com.dicodingsubmit.githubuser"
-    minSdk = 24
-    targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
+	defaultConfig {
+		applicationId = "com.dicodingsubmit.githubuser"
+		minSdk = 24
+		targetSdk = 34
+		versionCode = 1
+		versionName = "1.0"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-  }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
 
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
-  buildFeatures {
-    viewBinding = true
-  }
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+	buildFeatures {
+		viewBinding = true
+	}
 }
 
 dependencies {
 
-  implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("com.google.android.material:material:1.11.0")
-  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+	implementation("androidx.core:core-ktx:1.12.0")
+	implementation("androidx.appcompat:appcompat:1.6.1")
+	implementation("com.google.android.material:material:1.11.0")
+	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+	testImplementation("junit:junit:4.13.2")
+	androidTestImplementation("androidx.test.ext:junit:1.1.5")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-  // lottie files
-  implementation("com.airbnb.android:lottie:6.3.0")
+	// fragment
+	implementation("androidx.fragment:fragment-ktx:1.6.2")
 
-  // fragment
-  implementation("androidx.fragment:fragment-ktx:1.6.2")
+	// glide: display online image
+	implementation("com.github.bumptech.glide:glide:4.16.0")
+
+	// circleimageview: display circle image
+	implementation("de.hdodenhof:circleimageview:3.1.0")
+
+	// lottie files
+	implementation("com.airbnb.android:lottie:6.3.0")
+
+	//retrofit
+	implementation("com.squareup.retrofit2:retrofit:2.9.0")
+	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+	implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
 }
