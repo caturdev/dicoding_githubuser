f0e74e0e3f39ac37be82ea999fe685a60bc5e2db
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-02-28 11:17:15 +0700

    update search fragment

--- app/src/main/java/com/dicodingsubmit/githubuser/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/dicodingsubmit/githubuser/ui/fragment/SearchFragment.kt
@@ -5,29 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.dicodingsubmit.githubuser.R
 import com.dicodingsubmit.githubuser.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
 
-  private lateinit var binding: FragmentSearchBinding
+	private lateinit var binding: FragmentSearchBinding
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    // init binding and inflate the layout for this fragment
-    binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
-    return binding.root
-  }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		// init binding and inflate the layout for this fragment
+		binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+		return binding.root
+	}
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
 
-//    with(binding) {
-//      searchView.setupWithSearchBar(searchBar)
-//
-//    }
-  }
+		binding.backButton.setOnClickListener { v: View ->
+			parentFragmentManager.commit {
+				replace(R.id.frame_container, HomeFragment(), HomeFragment::class.java.simpleName)
+			}
+		}
+
+	}
 
 }
