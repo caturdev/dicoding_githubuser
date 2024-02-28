664da9c729853dc7da125a9ef97d6133ab12e660
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-02-28 13:37:03 +0700

    update search param back button

--- app/src/main/java/com/dicodingsubmit/githubuser/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/dicodingsubmit/githubuser/ui/fragment/HomeFragment.kt
@@ -11,28 +11,29 @@ import com.dicodingsubmit.githubuser.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-  private lateinit var binding: FragmentHomeBinding
+	private lateinit var binding: FragmentHomeBinding
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    // init binding and inflate the layout for this fragment
-    binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-    return binding.root
-  }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		// init binding and inflate the layout for this fragment
+		binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+		return binding.root
+	}
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
 
-    val fragmentManager = parentFragmentManager
-    val searchFragment = SearchFragment()
+		val fragmentManager = parentFragmentManager
+		val searchFragment = SearchFragment()
 
-    binding.fab.setOnClickListener { v: View ->
-      fragmentManager.commit {
-        replace(R.id.frame_container, searchFragment, HomeFragment::class.java.simpleName)
-      }
-    }
-  }
+		binding.fab.setOnClickListener { v: View ->
+			fragmentManager.commit {
+				replace(R.id.frame_container, searchFragment, HomeFragment::class.java.simpleName)
+				addToBackStack(null)
+			}
+		}
+	}
 }
--- app/src/main/java/com/dicodingsubmit/githubuser/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/dicodingsubmit/githubuser/ui/fragment/SearchFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.dicodingsubmit.githubuser.R
 import com.dicodingsubmit.githubuser.databinding.FragmentSearchBinding
+
 
 class SearchFragment : Fragment() {
 
@@ -26,7 +28,12 @@ class SearchFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
-		binding.backButton.setOnClickListener { v: View ->
+		(activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+		(activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+		(activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
+		(activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+
+		binding.toolbar.setNavigationOnClickListener {
 			parentFragmentManager.commit {
 				replace(R.id.frame_container, HomeFragment(), HomeFragment::class.java.simpleName)
 			}
