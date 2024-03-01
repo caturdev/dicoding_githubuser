c32bc2eff2b18569e4e19ecca59fdb909486f113
catur wicaksono <catur.wicaksono.developer@gmail.com>
2024-03-01 15:02:35 +0700

    add history local database

--- app/src/main/java/com/dicodingsubmit/githubuser/data/local/room/database/FavDatabase.kt
+++ app/src/main/java/com/dicodingsubmit/githubuser/data/local/room/database/FavDatabase.kt
@@ -5,12 +5,16 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.dicodingsubmit.githubuser.data.local.entity.FavEntity
+import com.dicodingsubmit.githubuser.data.local.entity.HistoryEntity
 import com.dicodingsubmit.githubuser.data.local.room.dao.FavDao
+import com.dicodingsubmit.githubuser.data.local.room.dao.HistoryDao
 
-@Database(entities = [FavEntity::class], version = 1)
+@Database(entities = [FavEntity::class, HistoryEntity::class], version = 1)
 abstract class FavDatabase : RoomDatabase() {
 
 	abstract fun favDao(): FavDao
+
+	abstract fun historyDao(): HistoryDao
 
 	companion object {
 		@Volatile
